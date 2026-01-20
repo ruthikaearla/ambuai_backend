858cb78439078c8fe549a84451d3ab0a7d206efa
Ruthika <ruthikaearla2gmail.com>
2026-01-20 22:27:19 +0530

    Add request_ambulance endpoint

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "AIAMBU backend running"}
+@app.post("/request_ambulance")
+def request_ambulance():
+    return {
+        "status": "searching",
+        "message": "Searching nearest hospital ambulance..."
+    }
