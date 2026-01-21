aae2a18cf338a53254c0757292c4eeb265685998
Ruthika <ruthikaearla2gmail.com>
2026-01-21 17:46:57 +0530

    Add request body validation

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# Page title
+st.set_page_config(page_title="AMBUAI - Fast Ambulance Service", layout="wide")
+st.title("🚑 AMBUAI - Fast Ambulance Service")
+st.markdown("Book an ambulance and get it to your location quickly!")
